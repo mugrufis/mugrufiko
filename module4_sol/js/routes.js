7bf679dbffb155cd282bd8e21dde0fde66a4a96a
mugrufis <siwrist@yahoo.gr>
2017-01-29 23:12:05 +0100

    should be working

--- module4_sol/js/routes.js
+++ module4_sol/js/routes.js
@@ -1,38 +1,4 @@
-// (function () {
-// 'use strict';
 
-// angular.module('MenuApp')
-// .config(RoutesConfig);
-
-// RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-// function RoutesConfig($stateProvider, $urlRouterProvider) {
-
-
-//   $urlRouterProvider.otherwise('/');
-
-  
-//   $stateProvider
-//   .state('home', {
-//     url: '/',
-//     templateUrl: 'js/templates/home.template.html'
-//   })
-
- 
-//   .state('categories', {
-//     url: '/categories',
-//     templateUrl: 'js/templates/categories.template.html',
-//     controller: 'CategoriesController as categoriesCtrl',
-//     resolve: {
-//       categories: ['MenuDataService', function(MenuDataService) {
-//         return MenuDataService.getAllCategories().then(function(response) {
-//           return response.data;
-//         });
-//       }]
-//     }
-// })
-// }
-
-// })();
 (function () {
 'use strict';
 
