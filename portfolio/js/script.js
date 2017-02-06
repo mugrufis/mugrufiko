f2b117e09179a52b812d918218bf26b2da815728
ntini <ntini@Mac-0248.local>
2017-02-06 12:10:04 +0100

    backup

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,37 +1,58 @@
-// $(document).ready(function() {
-//   function setHeight() {
-//     windowHeight = $(window).innerHeight();
-//     $('.ipsos').css('min-height', windowHeight);
-//   };
-//   setHeight();
-  
-//   $(window).resize(function() {
-//     setHeight();
-//   });
-// });
-
 $(document).ready(function(){
 	var controller = new ScrollMagic.Controller();
 
-	// var titleScene = new ScrollMagic.Scene({
-	// 	triggerElement:'#block2'
+	var myScene5 = new ScrollMagic.Scene({
+		triggerElement:'#block3',
+		triggerHook: 0.7
+	})
+	.setTween(TweenMax.staggerFrom('#block3 p', 1 , {
+		opacity:0,
+		scale:0,
+	}, 0.5 ))
+	.addIndicators()
+	.addTo(controller);
+	
+	var myScene4 = new ScrollMagic.Scene({
+		triggerElement:'#block3',
+		triggerHook: 0.5
+	})
+	.setTween(TweenMax.staggerFrom('#block3 img', 1 , {
+		opacity:0,
+		scale:0,
+		rotation:180
+	}, 0.5 ))
+	.addIndicators()
+	.addTo(controller);
 
-	// })
-	// .setPin('.title')
-	// .addTo(controller);
 
-	var myScene = new ScrollMagic.Scene({
+	var myScene3 = new ScrollMagic.Scene({
+    	triggerElement:'.aWord',
+    	reverse: false,
+    	triggerHook:0.8
+    })
+    .setClassToggle('.aWord','fade-in')
+	.addTo(controller);
+
+
+    var myScene2 = new ScrollMagic.Scene({
+    	triggerElement:'#block2 p',
+    	reverse: false,
+    	triggerHook:0.7
+    })
+    .setClassToggle('#block2 p','fade-in')
+	.addTo(controller);
+
+
+	var myScene1 = new ScrollMagic.Scene({
         triggerElement: '#block4',
         triggerHook: 1,
         reverse:false,
 
     })
 	.setClassToggle('#block4','fade-in')
-	.addIndicators()
 	.addTo(controller);
 
 });
-
 
 
 
